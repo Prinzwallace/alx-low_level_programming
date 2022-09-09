177985af0707c7918f5881daaf22fb96eacecc93
Prinzwallace <prinzk442@gmail.com>
2022-09-08 20:56:10 -0700

    T8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
