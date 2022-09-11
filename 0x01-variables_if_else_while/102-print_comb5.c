fbe8ab5a15d62dbedf90f40e6bf4c80422e2435c
Prinzwallace <prinzk442@gmail.com>
2022-09-10 18:29:05 -0700

    Finally oo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int p, q;
+
+	for (p = 0; p <= 98; p++)
+	{
+		for (q = p + 1; q <= 99; q++)
+		{
+			putchar((p / 10) + '0');
+			putchar((p % 18) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + 10);
+
+			if (p == 98 && q == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
