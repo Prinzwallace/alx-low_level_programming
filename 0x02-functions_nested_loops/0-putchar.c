57c1fe5003db8c1f8b84d08107ee3092c4eae6ed
Prinzwallace <prinzk442@gmail.com>
2022-09-13 06:10:59 -0700

    7

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,7 +9,12 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	char word[4] = "main";
+	int i;
+
+	for (i = 0; i < 4; i++)
+		_putchar(word[i]);
+	_putchar('\n');
 
 	return (0);
 }
