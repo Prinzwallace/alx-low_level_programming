cee9ef171ec3aaa458d3448243038bae734fbbec
Prinzwallace <prinzk442@gmail.com>
2022-09-14 09:09:11 -0700

    9

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: n is an interger
+ * Description: e dey print value of integer sha
+ * Return: int
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
