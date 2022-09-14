f5853f1d023abfcf11c160944a98f73620dbbd3d
Prinzwallace <prinzk442@gmail.com>
2022-09-14 16:02:30 -0700

    9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds integers
+ * @n1: integerb1
+ * @n2: integer 2
+ * Description: sums
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
