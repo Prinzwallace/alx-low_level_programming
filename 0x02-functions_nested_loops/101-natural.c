37aaf6b304aebdd10e0d2144e65717ea0f560b02
Prinzwallace <prinzk442@gmail.com>
2022-09-14 17:19:20 -0700

    7

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all sums kf multiple 3 and 5
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 9) || (i % 5 == 0))
+		{
+			z += i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
+}
